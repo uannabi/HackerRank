73e9c60eb16f2fc920b70f5869f5bfd0aa692915
uannabi <zahid_sc@hotmail.fr>
2021-09-19 23:38:42 +0600

    reversed linkedlist

--- DataType/linkList.py
+++ DataType/linkList.py
@@ -29,8 +29,37 @@ class LinkedList:
       current = current.next
 
 # Singly Linked List with insertion and print methods
+#LL = LinkedList()
+#LL.insert(3)
+#LL.insert(4)
+#LL.insert(5)
+#LL.printLL()
+
+def reverseList(list):
+  # initialize variables
+  previous = None         # `previous` initially points to None
+  current = list.head     # `current` points at the first element
+  following = current.next    # `following` points at the second element
+
+  # go till the last element of the list
+  while current:
+      current.next = previous # reverse the link
+      previous = current      # move `previous` one step ahead
+      current = following         # move `current` one step ahead
+      if following:               # if this was not the last element
+          following = following.next    # move `following` one step ahead
+
+  list.head = previous
+
+
+# Testing
 LL = LinkedList()
+LL.insert(1)
+LL.insert(2)
 LL.insert(3)
 LL.insert(4)
-LL.insert(5)
+print("Linked List")
+LL.printLL()
+print("Reversed Linked List")
+reverseList(LL)
 LL.printLL()
